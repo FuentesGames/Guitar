b0b5ed8a4856426f58f621f0c5e991eff1f53976
FuentesGames <mr.james.fuentes@gmail.com>
2016-09-02 19:20:24 -0400

    Allmost working

---
+++ Assets/Scripts/Progress Bar/BarScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class BarScript : MonoBehaviour {
+
+	[SerializeField]
+	private float FillAmmount;
+
+	[SerializeField]
+	private Image content;
+
+	public float MaxValue { get; set; }
+
+	public float Value
+	{
+		set {
+			FillAmmount = Map (value, 0, MaxValue, 0, 1);
+		}
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		HandleBar ();
+	
+	}
+
+	private void HandleBar()
+	{
+			content.fillAmount = FillAmmount;
+		}
+
+
+	private float Map(float value, float inMin, float inMax, float outMin, float outMax){
+		return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
+		//Current Level (78 - 0) * (1 - 0) / (230 - 0) + 0 
+	}
+}
---
+++ Assets/Scripts/Progress Bar/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private Stat level;
+
+	private void Awake()
+	{
+		level.Initalize ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+			{
+				level.CurrentVal -= 10;
+			}
+		if (Input.GetKeyDown (KeyCode.Y))
+			{
+				level.CurrentVal += 10;
+			}
+
+	}
+}
